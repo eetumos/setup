c67275ae109881490488e537778494c6bcce1adb
Eetu Suihkonen <eetu.suihkonen@gmail.com>
2025-12-08 20:03:43 +0200

    Stay on NVIDIA 580 driver

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-find $(dirname $0)/{files-nvidia/etc/nvidia/kernel.conf,build-env,patches} -exec touch -d2025-01-01 {} +
+find $(dirname $0)/{build-env,patches} -exec touch -d2025-01-01 {} +
 
 podman build --target=base   -t ghcr.io/eetumos/silverblue:latest --pull=never "$@" .
 podman build --target=base   -t ghcr.io/eetumos/silverblue:base   --pull=never "$@" .
